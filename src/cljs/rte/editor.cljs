4e6f032d768022c8fad1050cbcb1f4b6d8d9a298
Paul Cristian Martin <paul931224@gmail.com>
2021-02-26 19:09:27 +0100

    Event listeners now come and go with focus and blur.

--- src/cljs/rte/editor.cljs
+++ src/cljs/rte/editor.cljs
@@ -29,6 +29,15 @@
 (defn add-time-out [func timeout]
   (.setTimeout js/window func timeout))
 
+(defn insert-in-vector [coll index value]
+  (if-not (empty? coll)
+    (vec (concat (subvec coll 0 index)
+           [value]
+           (subvec coll index)))))
+
+(defn insert-in-string [string position value]
+  (apply str (insert-in-vector (mapv #(str %) string) position (str value))))
+
 ;;;;;;;;;;;;;;
 ;;;;Config;;;;
 ;;;;;;;;;;;;;;
@@ -53,15 +62,22 @@
 ;;;;Events;;;;
 ;;;;;;;;;;;;;;
 
-(defn set-cursor-state [new]
+(defn set-cursor-state! [new]
   (reset! cursor-state (merge @cursor-state new)))
 
 (defn set-editor-state [new]
   (reset! text-editor-state (merge @text-editor-state new)))
 
 (defn update-editor-state [path new]
-   (reset! text-editor-state (assoc-in @text-editor-state path new)))
+  (reset! text-editor-state (assoc-in @text-editor-state path new)))
 
+(def empty-block {:content "\u00A0"
+                  :type    :div})
+
+(defn add-block-to-editor [index block]
+  (reset! text-editor-state
+    (insert-in-vector @text-editor-state
+      index block)))
 ;;;;;;;;;;;;;;;;;;;;
 ;;;;Editor utils;;;;
 ;;;;;;;;;;;;;;;;;;;;
@@ -115,7 +131,7 @@
             [start-block end-block] (get-block-boundaries the-range)]
         ;We check if it is a text-node, we are interested in only those.
         (if is-text-node?
-          (set-cursor-state
+          (set-cursor-state!
             {:start       start
              :end         end
              :start-block (int start-block)
@@ -125,65 +141,62 @@
   (let [selection (.getSelection js/window)]
     (add-time-out #(.collapse selection (get-block-node row) column) 0)))
 
-(def empty-block {:content "\u00A0"
-                  :type    :div})
-
-(defn insert-in-vector [coll index value]
-  (if-not (empty? coll)
-    (vec (concat (subvec coll 0 index)
-           [value]
-           (subvec coll index)))))
-
-(defn add-block-to-editor [index block]
-  (reset! text-editor-state
-    (insert-in-vector @text-editor-state
-      index block)))
-
-
-
-
-
-
-
-
-
-
 
 ;;;;;;;;;;;;;
 ;;Listeners;;
 ;;;;;;;;;;;;;
 
-(defn enter-listener []
-  (add-event-listener
-    (text-editor-dom)
-    "keydown"
-    (fn [e]
-      (if (= (.-which e) 13)
-        (let [id (inc (:start-block @cursor-state))]
-          (do
-            (.preventDefault e)
-            (swap! cursor-state assoc :start-block id)
-            (add-block-to-editor id empty-block)
-            (add-time-out #(set-cursor-to-block-start id) 0)))))))
+(defn enter-listener [e]
+  (if (= (.-which e) 13)
+    (let [id (:start-block @cursor-state)]
+      (do
+        (.preventDefault e)
+        (set-cursor-state! {:start-block (inc id)})
+        (add-block-to-editor (inc id) empty-block)
+        (add-time-out #(set-cursor-to-block-start id) 0)))))
 
-(defn input-listener []
-  (add-event-listener
-    (text-editor-dom)
-    "beforeinput"
-    (fn [event]
-      (let [the-keys [(:start-block @cursor-state) :content]]
-         (do
-           (.removeAllRanges (get-selection-object))
-           (update-editor-state the-keys (str (get-in @text-editor-state the-keys) (.-data event)))
-           (set-cursor-state {:start (inc (:start @cursor-state))}))))))
+(defn add-enter-listener []
+  (add-event-listener (text-editor-dom) "keydown" enter-listener))
 
-(defn selection-listener []
-  (add-event-listener
-    js/document "selectionchange"
-    (fn [e]
-      (log "selection changed" e)
-      (.preventDefault e)
-      (set-range))))
+(defn remove-enter-listener []
+  (remove-event-listener (text-editor-dom) "keydown" enter-listener))
+
+(defn input-listener [event]
+  (let [the-keys [(:start-block @cursor-state) :content]]
+    (do
+      (.removeAllRanges (get-selection-object))
+      (update-editor-state the-keys (insert-in-string
+                                      (get-in @text-editor-state the-keys)
+                                      (:start @cursor-state)
+                                      (.-data event)))
+      (set-cursor-state! {:start (inc (:start @cursor-state))}))))
+
+(defn add-input-listener []
+  (add-event-listener (text-editor-dom) "beforeinput" input-listener))
+
+(defn remove-input-listener []
+  (remove-event-listener (text-editor-dom) "beforeinput" input-listener))
+
+(defn selection-listener [e]
+  (do
+    (.preventDefault e)
+    (set-range)))
+
+(defn add-selection-listener []
+  (add-event-listener js/document "selectionchange" selection-listener))
+
+(defn remove-selection-listener []
+  (remove-event-listener js/document "selectionchange" selection-listener))
+
+(defn add-all-editor-listeners []
+  (do (add-selection-listener)
+      (add-input-listener)
+      (add-enter-listener)))
+
+(defn remove-all-editor-listeners []
+  (do (remove-selection-listener)
+      (remove-input-listener)
+      (remove-enter-listener)))
 
 ;;;;;;;;;;
 ;;;;UI;;;;
@@ -213,36 +226,44 @@
   [:<>
    (for [[id {:keys [type content]}] (map-indexed #(vector %1 %2) state)]
      (case type
-       :div ^{:key id} [:div {:data-block id} content]
+       :div ^{:key id} [:div {:style      {:white-space "pre"}
+                              :data-block id} content]
        [non-existing]))])
 
 ;The content editable, which renders the edn structure in hiccup.
+
+(defn all-listeners []
+  (do
+    (add-enter-listener)
+    (add-input-listener)
+    (add-selection-listener)
+    (.focus (get-element-by-id text-editor-id))))
+
+
 (defn content-editable []
-  (let [style {:background "#222" :overflow-y "auto"}]
+  (let [last-block (atom 0)
+        style      {:background "#222" :overflow-y "auto"}]
     (r/create-class
       {:component-did-update
-       #(set-cursor-position (:start-block @cursor-state) (:start @cursor-state))
+       (fn []
+         (if (= @last-block (:start-block @cursor-state))
+           (set-cursor-position (:start-block @cursor-state) (:start @cursor-state))
+           (set-cursor-position (:start-block @cursor-state) 0))
+         (reset! last-block (:start-block @cursor-state)))
        :reagent-render
        (fn []
          [:div {:style style}
           [:div {:content-editable                  true
                  :id                                text-editor-id
+                 :on-focus                          #(add-all-editor-listeners)
+                 :on-blur                           #(remove-all-editor-listeners)
                  :style                             {:padding "10px" :color "white" :height "500px"}
                  :suppress-content-editable-warning true}
            [edn->rich-text @text-editor-state]]])})))
 
 ;The text editor, with some debugging tools
 (defn text-editor []
-  (r/create-class
-    {:component-did-mount                                   ;Mounting the listeners for modifying, and autofocus
-     #(do
-        (enter-listener)
-        (input-listener)
-        (selection-listener)
-        (.focus (get-element-by-id text-editor-id)))
-     :reagent-render
-     (fn []
-       [:div
-        [display-cursor-state @cursor-state]
-        [display-editor-state @text-editor-state]
-        [content-editable]])}))
+  [:div
+   [display-cursor-state @cursor-state]
+   [display-editor-state @text-editor-state]
+   [content-editable]])
